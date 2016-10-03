59001b56b29ae2d792c70eb3d30a673766b24dfe
Flatiron School <flatironschool@Flatirons-MacBook-Air-67.local>
2016-10-03 12:04:26 -0400

    first commit

---
+++ ios-0916-generic-extensions/GenericExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  GenericExtensions.swift
+//  ios-0916-generic-extensions
+//
+//  Created by Flatiron School on 10/3/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
