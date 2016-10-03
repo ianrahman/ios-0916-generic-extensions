87cb9a732510208e8843a4b98b62507999c6c9d0
Flatiron School <flatironschool@Flatirons-MacBook-Air-67.local>
2016-10-03 12:07:52 -0400

    Oops

--- ios-0916-generic-extensions/GenericExtensions.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GenericExtensions.swift
-//  ios-0916-generic-extensions
-//
-//  Created by Flatiron School on 10/3/16.
-//  Copyright © 2016 Flatiron School. All rights reserved.
-//
-
-import Foundation
