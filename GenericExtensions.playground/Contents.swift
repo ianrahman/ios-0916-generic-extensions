0d55b525d18fd30fd0daffc99bdfc84998f6943d
Christopher Boynton <flatironschool@Flatirons-MacBook-Air-67.local>
2016-10-14 12:14:40 -0400

    Added Array Extensions

--- GenericExtensions.playground/Contents.swift
+++ GenericExtensions.playground/Contents.swift
@@ -1,12 +1,45 @@
 //iOS Generic Extensions
 import Foundation
 
-// Return a random integer, between a min and a max number, inclusive
-// By Ben Su and Chris Boynton
-// Created 10/3/16
-func randomInt(from min: Int, to max: Int) -> Int {
-    return Int(arc4random_uniform(UInt32(max - min + 1))) + min
+///////////
+/// Int ///
+///////////
+extension Int {
+    
+    // Return the correct ordinal string (th, st, nd, rd) for an Integer
+    // By Flatiron Staff
+    // Created 10/14/16
+    func ordinal() -> String {
+        var ordinal = "th"
+        if self % 10 == 1 && self % 100 != 11 {
+            ordinal = "st"
+        } else if self % 10 == 2 && self % 100 != 12 {
+            ordinal = "nd"
+        } else if self % 10 == 3 && self % 100 != 13 {
+            ordinal = "rd"
+        }
+        return ordinal
+    }
 }
 
+/////////////
+/// Array ///
+/////////////
 
 
+extension Array {
+    
+    // Return a random element from an array
+    // By Chris Boynton
+    // Created 10/14/16
+    func returnRandomElement() -> Element {
+        return self[Int(arc4random_uniform(UInt32(self.count)))]
+    }
+    
+    // Remove an element from an array, and return that element
+    // By Chris Boynton
+    // Created 10/14/16
+    mutating func removeRandomElement() -> Element {
+        return self.remove(at: Int(arc4random_uniform(UInt32(self.count))))
+    }
+}
---
+++ GenericFunctions.playground/Contents.swift
@@ -0,0 +1,9 @@
+//iOS Generic Functions
+import Foundation
+
+// Return a random integer, between a min and a max number, inclusive
+// By Ben Su and Chris Boynton
+// Created 10/3/16
+func randomInt(from min: Int, to max: Int) -> Int {
+    return Int(arc4random_uniform(UInt32(max - min + 1))) + min
+}
